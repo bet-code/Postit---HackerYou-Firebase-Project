fb87bbaf57a29c5a7530dcb2d2eb76dd2be8d770
Gilbert <gilbert.j5@outlook.com>
2017-11-27 07:33:59 -0500

    Made Title input as a required field to fill in

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -81,7 +81,7 @@ class App extends React.Component {
           <main>
             <section className="inputNotes">
               <form onSubmit={this.postNote}>
-                <input type="text" name='title' autoComplete="off" placeholder="Enter Title" value={this.state.title} onChange={this.handleChange}/>
+                <input type="text" required name='title' autoComplete="off" placeholder="Enter Title" value={this.state.title} onChange={this.handleChange}/>
                 <textarea name="comment" autoComplete="off" placeholder="Enter comments..." cols="30" rows="10" value={this.state.comment} onChange={this.handleChange}></textarea>
                 <button>POST IT</button>
               </form>
